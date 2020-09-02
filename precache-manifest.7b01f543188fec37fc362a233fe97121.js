78d8067e12ed04cf1298ac9db5f6ed4df16bb67e
anonymous <anonymous@example.com>
2020-09-02 10:17:39 +0200

    Updates

---
+++ precache-manifest.7b01f543188fec37fc362a233fe97121.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f68bb2517565cd377dbb2cf45766934",
+    "url": "/covermaker/index.html"
+  },
+  {
+    "revision": "3cbf8c4ad7262922f576",
+    "url": "/covermaker/static/js/2.6fcea19b.chunk.js"
+  },
+  {
+    "revision": "47da391ea7f537311643b0a9b0caae55",
+    "url": "/covermaker/static/js/2.6fcea19b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdb1359c8402611a6ca2",
+    "url": "/covermaker/static/js/main.c87fdd22.chunk.js"
+  },
+  {
+    "revision": "3df6bfb82d7f1ebb515d",
+    "url": "/covermaker/static/js/runtime-main.920adc40.js"
+  }
+]);
